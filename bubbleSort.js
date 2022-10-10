cbc0f4ab5a2745c801f0460a35c3a06794e0e087
AmitKumar <hackak4444@gmail.com>
2022-10-10 16:14:09 +0530

    optimised bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -11,12 +11,15 @@ let n = arr.length;
 
 function bubbleSort(n,arr) {
     for(let i=0;i<n-1;i++) {
+        let swapped = false;
         for(let j=0;j<n-1-i;j++) {
             if(arr[j]>arr[j+1]) {
-                let temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                swapped = true;
+                swap(arr,j+1,j)
             }
+        }
+        if(!swapped) {
+            break;
         }
     }
     return arr;
---
+++ insertionSort.js
@@ -0,0 +1,5 @@
+let arr = [5, 6, 2, 4, 3, 9];
+
+for(let i=1;i<n;i++) {
+    // for(let j=0)
+}

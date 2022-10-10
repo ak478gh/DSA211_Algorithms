b0cc2eb77599db07c162751266047ae829c05836
AmitKumar <hackak4444@gmail.com>
2022-10-10 19:21:23 +0530

    insertion sort added

--- insertionSort.js
+++ insertionSort.js
@@ -1,5 +1,12 @@
 let arr = [5, 6, 2, 4, 3, 9];
-
+let n = arr.length;
 for(let i=1;i<n;i++) {
-    // for(let j=0)
+    let temp = arr[i];
+    let j = i-1;
+    while(j>=0 && arr[j]>temp) {
+        arr[j+1] = arr[j];
+        j--;
+    }
+    arr[j+1] = temp;
 }
+console.log(arr);

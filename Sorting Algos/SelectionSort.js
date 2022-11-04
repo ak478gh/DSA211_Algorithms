1d2f4c3f1e4318b4c2b7ffe4a15a5bc5ac164689
ak478gh <amitkumar655921@gmail.com>
2022-11-04 19:21:59 +0530

    made correction

--- Sorting Algos/SelectionSort.js
+++ Sorting Algos/SelectionSort.js
@@ -2,13 +2,13 @@ let arr = [1, 3, 4, 6, 5];
 
 function selectionSort(arr) {
     let n = arr.length;
-    let count = 0;
     for(let i=0;i<n;i++) {
+
         let swapped = false;
         for(let j=0;j<n-i;j++) {
+            
             if(arr[j]>arr[j+1]) {
                 swapped = true;
-                count++;
                 swap(arr, j, j+1)
             }
         }
